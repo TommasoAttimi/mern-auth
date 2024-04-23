8b07348fbf6c2fec8745e0d0d000cc65d0f3a5f0
Tommaso Tadiello <tommaso.attimi@pm.me>
2024-04-23 15:26:57 +0200

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js";
+import userRoute from "./routes/userRoute.js";
+import authRoute from "./routes/authRoute.js";
 dotenv.config();
 
 mongoose
@@ -15,8 +16,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
-app.use("/api/user", userRoutes);
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
